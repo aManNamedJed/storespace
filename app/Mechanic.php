626ce8eda98e460e8d543e4a402d4488c6b0cc74
Jed Darrohn <jed@Jeds-MacBook-Pro-2.local>
2019-11-03 22:15:59 -0700

    Clean up unneeded code in Customer and Mechanic after extending User.

--- app/Mechanic.php
+++ app/Mechanic.php
@@ -13,33 +13,6 @@ class Mechanic extends User
     protected $table = 'users';
 
     /**
-     * The attributes that are mass assignable.
-     *
-     * @var array
-     */
-    protected $fillable = [
-        'first_name','last_name', 'email', 'password', 'phone',
-    ];
-
-    /**
-     * The attributes that should be hidden for arrays.
-     *
-     * @var array
-     */
-    protected $hidden = [
-        'password', 'remember_token',
-    ];
-
-    /**
-     * The attributes that should be cast to native types.
-     *
-     * @var array
-     */
-    protected $casts = [
-        'email_verified_at' => 'datetime',
-    ];
-
-    /**
      * The Mechanic's Appointments
      *
      * @return mixed
